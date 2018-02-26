1ceae0709378535035d75e27558d26193032e7e2
Nicholas Biddell <nicbiddell@gmail.com>
2018-02-25 20:14:31 -0500

    Route to stub components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { UploadCsvComponent } from './upload-csv/upload-csv.component';
+import { DocumentListComponent } from './document-list/document-list.component';
 
 const routes: Routes = [
   { path: 'dashboard', component: DashboardComponent},
+  { path: 'upload', component: UploadCsvComponent },
+  { path: 'doc-list', component: DocumentListComponent },
   { path: '', redirectTo: '/dashboard', pathMatch: 'full'}
 ];
 
