4ccac322ef6747d1d277a94c212df3dcac6342a1
Nicholas Biddell <nicbiddell@gmail.com>
2018-03-06 21:49:44 -0500

    Dynamically dispay headers / data for a given CSV

--- src/app/document-detail/document-detail.component.ts
+++ src/app/document-detail/document-detail.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnChanges, Input } from '@angular/core';
+import { Observable } from 'rxjs';
 
 import { Document } from '../document.model';
 import { DocumentService } from '../document.service';
@@ -12,26 +13,41 @@ export class DocumentDetailComponent implements OnChanges {
 
   @Input()
   document: Document
-  data;
-  headers: string[];
+  data$: string[] = new Array<string>();
+  headers: string[] = new Array<string>();
 
   constructor(private docService: DocumentService) { }
 
   ngOnChanges() {
-    this.getDataForDocument();
+    if (this.document) {
+      this.getDataForDocument();
+    }
   }
 
   getDataForDocument(): void {
     this.docService.getDocumentDetail(this.document.pk)
-        .subscribe(
-          res => { 
-            this.data = JSON.stringify(res);
-           }
-        );
-  }
-  
-  getHeaders(): void {
-      this.headers = this.data[0];
-  }
+      .subscribe(
+        res => {
+          this.data$ = res;
 
-}
+          const first_obj = res[0];
+
+          let currData: Array<any> = new Array<any>();
+
+          for (let obj of res) {
+            let curr_obj_data = new Array<any>();
+            for (let field in obj) {
+              curr_obj_data.push(obj[field]);
+            }
+            currData.push(curr_obj_data);
+          }
+          this.data$ = currData;
+
+          for (let header in first_obj) {
+              this.headers.push(header);             
+          }
+        }
+      );
+  }
+};
+
